76463124fd0bd8f276394af55add8674c6d138d1
Ernani da Costa <ernanidacosta@gmail.com>
2020-07-17 12:34:01 -0300

    .

--- pypro/settings.py
+++ pypro/settings.py
@@ -145,8 +145,8 @@ if AWS_ACCESS_KEY_ID:
     AWS_QUERYSTRING_AUTH = True
     AWS_S3_CUSTOM_DOMAIN = None
 
-    COLLECTFAST_ENABLED = True
     COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+    COLLECTFAST_ENABLED = True
 
     AWS_DEFAULT_ACL = 'private'
 
