fc84731ad92854f7fc4dd07e4488bcf1abb32c25
Ernani da Costa <ernanidacosta@gmail.com>
2020-08-30 00:29:32 -0300

    some updates

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
+
 def video(request, slug):
-   return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': 447357180}
+    return render(request, 'aperitivos/video.html', context={'video': video})
